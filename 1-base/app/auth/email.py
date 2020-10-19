df1bdd666b35903b5fc1d172f0a59481796bb9a5
pauljohn <pjohnson@teracrunch.com>
2020-10-19 13:10:17 -0500

    auth/email.py set template for outgoing messages to auth/template folder

--- 1-base/app/auth/email.py
+++ 1-base/app/auth/email.py
@@ -1,5 +1,4 @@
 from flask import render_template, current_app
-## from flask_babel import _
 from app.email import send_email
 
 
@@ -8,7 +7,7 @@ def send_password_reset_email(user):
     send_email('Reset Your Password',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
-               text_body=render_template('email/reset_password.txt',
+               text_body=render_template('auth/outgoing_reset_password.txt',
                                          user=user, token=token),
-               html_body=render_template('email/reset_password.html',
+               html_body=render_template('auth/outgoing_reset_password.html',
                                          user=user, token=token))
