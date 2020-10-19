f099b30d301556e73ebb389e012e42e20ee39e7c
pauljohn <pjohnson@teracrunch.com>
2020-10-19 11:50:49 -0500

    app/__init__.py: put in error logging setup_notes.md: new name for top level commentary

--- 1-base/app/__init__.py
+++ 1-base/app/__init__.py
@@ -1,6 +1,8 @@
 import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+from flask import Flask, current_app
 
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -8,22 +10,17 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 
 
-
 #from config import Config
-
 ## app = Flask(__name__)
-
 print("OS environment", os.environ.get("FLASK_ENV"))
 
 
 db = SQLAlchemy()
 migrate = Migrate()
-
 login = LoginManager()
 login.login_view = 'auth.login'
 login.login_message = "Please log in"
 bootstrap = Bootstrap()
-
 mail = Mail()
 
 def create_app():
@@ -50,11 +47,52 @@ def create_app():
     #     app.register_blueprint(main.main_bp)
 
     #     return app
-    print("app.register now:")
+    print("Entering app.register phase now:")
+    from app.errors import errors_bp
+    app.register_blueprint(errors_bp)
+
     from app.auth import auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
+
     from app.main import main_bp
     app.register_blueprint(main_bp)
+
+    # PJ 20201019: dont disable MAIL and logging
+    # if not app.debug and not app.testing:
+    if 1:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'],
+                        app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='App Failure',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/microblog.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('App startup')
 
     return app    
 
